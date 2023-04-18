feee9f17442af2f282139397a156df70f4d6eefb
Taline Chuman <talinechuman@hotmail.com>
2023-04-17 21:50:27 -0300

    Correções

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
-// import Sidebar from './components/Sidebar';
+import Sidebar from './components/Sidebar';
 import Content from './components/Content';
 import Header from './components/Header';
 import './App.css';
-import Login from './pages/Login';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Header />
-        <Login />
+        <p>TrybeTunes</p>
+        <Sidebar />
         <Content />
+        <Header />
       </div>
     );
   }
 }
-
 export default App;
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -4,36 +4,36 @@ import Loading from '../components/Loading';
 class Search extends React.Component {
   state = {
     searchInput: '',
-    loading: false,
+    loadingSearch: false,
   };
 
-  validateButton = () => {
+  validateButtonSearch = () => {
     const { searchInput } = this.state;
     const numMin = 2;
     return searchInput.length < numMin;
   };
 
   render() {
-    const { searchInput, loading } = this.state;
+    const { searchInput, loadingSearch } = this.state;
     return (
-      <div data-testid="page-Search">
+      <div data-testid="page-search">
         <p>Search</p>
         <input
           data-testid="search-artist-input"
           type="text"
           value={ searchInput }
-          name="name"
+          name="nameSearch"
           onChange={ (event) => this.setState({ searchInput: event.target.value }) }
         />
         <button
           data-testid="search-artist-button"
           type="button"
-          disabled={ this.validateButton() }
+          disabled={ this.validateButtonSearch() }
           // onClick={ this.handleCreateUser }
         >
-          Entrar
+          Pesquisar
         </button>
-        {loading && <Loading />}
+        {loadingSearch && <Loading />}
         {/* // Exibe o componente "Loading" enquanto "loading" for verdadeiro */}
       </div>
     );
