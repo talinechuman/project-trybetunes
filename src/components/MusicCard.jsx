11ce85ecc3b4c05f7d556524699dbab4e9a804b0
Taline Chuman <talinechuman@hotmail.com>
2023-04-27 20:16:19 -0300

    Requisito 08 corrigido, e 10 implementado

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+// import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 class MusicCard extends React.Component {
   state = {
